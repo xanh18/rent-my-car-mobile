0435ae458a1eae86aae5beabafa6db7372387f65
ccmavans <72778596+ccmavans@users.noreply.github.com>
2021-12-16 18:37:30 +0100

    General app structure attempt 1.1

--- app/src/main/java/com/example/rentmycar/api/TripAPI.kt
+++ app/src/main/java/com/example/rentmycar/api/TripAPI.kt
@@ -3,8 +3,23 @@ package com.example.rentmycar.api
 import com.example.rentmycar.model.Trip
 import retrofit2.Call
 import retrofit2.http.GET
+import java.util.*
 
 interface TripAPI {
+
+    @GET("trip")
+    fun findAll(): Call<List<Trip>>
+
     @GET("trip/person/1")
-    fun getData(): Call<List<Trip>>
+    fun findByUser_Id(): Call<List<Trip>>
+
+    @GET("trip/car/1")
+    fun findByCar_Id(): Call<List<Trip>>
+
+    @GET("trip/1")
+    fun findById(): Call<Optional<Trip>>
+
+
+
+
 }
