3105cbd2860b50d86717ee872f0abbfece7cdad6
Outis073 <72567157+Outis073@users.noreply.github.com>
2021-12-25 14:04:32 +0100

    p

--- app/src/main/java/com/example/rentmycar/api/CarAPI.kt
+++ app/src/main/java/com/example/rentmycar/api/CarAPI.kt
@@ -1,4 +1,19 @@
 package com.example.rentmycar.api
 
+import com.example.rentmycar.model.Car
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.POST
+import java.util.*
+
 interface CarAPI {
+
+    @GET("car")
+    fun findAll(): Call<List<Car>>
+
+    @GET("car/get/{id}")
+    fun findById(): Call<Optional<Car>>
+
+    @POST("car/save")
+    fun saveCar(): Call<Car>
 }
